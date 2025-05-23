1615a6590d86276c8fbcdb0089c1dc8164c4d942
cantbecaught <rvlzpg@gmail.com>
2025-05-24 04:57:50 +0700

    Update login.js

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,13 +1,18 @@
 import db from '../../utils/db.js';
 import { compare } from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+
 export default async function handler(req, res) {
+  if (req.method !== 'POST') return res.status(405).end();
   const { username, password } = req.body;
+  
   const client = await db();
   const user = await client.db().collection('users').findOne({ username });
   if (!user) return res.json({ message: 'User tidak ditemukan' });
+  
   const match = await compare(password, user.password);
   if (!match) return res.json({ message: 'Password salah' });
+  
   const token = jwt.sign({ username }, process.env.JWT_SECRET);
   res.setHeader('Set-Cookie', `token=${token}; Path=/`);
   res.json({ message: 'Login berhasil' });
