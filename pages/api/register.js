cfd298d9f1d09b2240d05c194ed3547535a6bf4c
cantbecaught <rvlzpg@gmail.com>
2025-05-24 04:58:36 +0700

    Update register.js

--- pages/api/register.js
+++ pages/api/register.js
@@ -1,10 +1,15 @@
 import db from '../../utils/db.js';
 import { hash } from 'bcryptjs';
+
 export default async function handler(req, res) {
+  if (req.method !== 'POST') return res.status(405).end();
   const { username, password } = req.body;
+  
   const client = await db();
+  
   const user = await client.db().collection('users').findOne({ username });
   if (user) return res.json({ message: 'Username sudah terdaftar' });
+  
   await client.db().collection('users').insertOne({ username, password: await hash(password, 10) });
   res.json({ message: 'Registrasi berhasil' });
 }
