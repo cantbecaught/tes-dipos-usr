c0e61f5f124bb5be817b4d6a1fef6f33d5c4d854
cantbecaught <rvlzpg@gmail.com>
2025-05-24 04:18:05 +0700

    Create auth.js

---
+++ utils/auth.js
@@ -0,0 +1,13 @@
+import jwt from 'jsonwebtoken';
+
+export function signToken(user) {
+  return jwt.sign({ id: user._id, username: user.username }, process.env.JWT_SECRET, { expiresIn: '1d' });
+}
+
+export function verifyToken(token) {
+  try {
+    return jwt.verify(token, process.env.JWT_SECRET);
+  } catch {
+    return null;
+  }
+}
